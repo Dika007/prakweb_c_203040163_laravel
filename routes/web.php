baf1e18a073cbee2acfaad6bc66a5c432cf9e0fa
Dika <dsa799059@gmail.com>
2022-10-17 20:50:31 +0700

    Membuat struktur folder routers dan view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    //return view('welcome');
+    return view('about', [
+        "name" => "DSA",
+        "email" => "Dark.gamil.com",
+        "image" => "Fotoku.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    //return view('welcome');
+    return view('posts');
 });
