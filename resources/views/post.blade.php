a10de776c808266448bbe1e66bec9902b99bad9b
Dika <dsa799059@gmail.com>
2022-10-26 02:48:54 +0700

    Post Category dan Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,9 @@
 @section('container')
 <h2>{{ $post->title }}</h2>
 
+<p>By. Dikz in
+  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
 {!! $post->body !!}
 <a href="/blog">back to Posts</a>
 @endsection
