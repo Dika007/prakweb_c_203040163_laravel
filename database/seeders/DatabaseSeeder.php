6a4abb4a5a8cde4f62f1329ab80aeab5ee5373e9
Dika <dsa799059@gmail.com>
2022-10-26 03:35:50 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +23,63 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Dikz',
+            'email' => 'DKZ@gmail.com',
+            'password' => bcrypt('sepakbola')
+        ]);
+
+        User::create([
+            'name' => 'DKZ',
+            'email' => 'ZZZZ@gmail.com',
+            'password' => bcrypt('sepakbola321')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programing',
+            'slug' => 'Web-programing'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'lorem pertama dibuat',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore dolores consectetur atque iure dolor maxime dolorum expedita aliquam recusandae dolore natus rem,</p><p>ab autem pariatur magni architecto eligendi quaerat cumque incidunt corrupti? Consequuntur odit assumenda dicta veritatis vel delectus temporibus labore necessitatibus quae eum consequatur sed ipsa amet facilis,</p><p>accusamus esse minus architecto cumque fugit voluptate. Impedit deleniti, animi architecto recusandae earum nostrum ad harum aperiam numquam ut voluptate enim. Officia consequatur, iste iusto harum nostrum aperiam incidunt nobis provident praesentium rerum saepe, quae eius voluptatem et laudantium molestias molestiae modi neque. Repudiandae, reiciendis eveniet temporibus minima nesciunt accusantium quae.</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'lorem kedua dibuat',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore dolores consectetur atque iure dolor maxime dolorum expedita aliquam recusandae dolore natus rem,</p><p>ab autem pariatur magni architecto eligendi quaerat cumque incidunt corrupti? Consequuntur odit assumenda dicta veritatis vel delectus temporibus labore necessitatibus quae eum consequatur sed ipsa amet facilis,</p><p>accusamus esse minus architecto cumque fugit voluptate. Impedit deleniti, animi architecto recusandae earum nostrum ad harum aperiam numquam ut voluptate enim. Officia consequatur, iste iusto harum nostrum aperiam incidunt nobis provident praesentium rerum saepe, quae eius voluptatem et laudantium molestias molestiae modi neque. Repudiandae, reiciendis eveniet temporibus minima nesciunt accusantium quae.</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'lorem ketiga dibuat',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore dolores consectetur atque iure dolor maxime dolorum expedita aliquam recusandae dolore natus rem,</p><p>ab autem pariatur magni architecto eligendi quaerat cumque incidunt corrupti? Consequuntur odit assumenda dicta veritatis vel delectus temporibus labore necessitatibus quae eum consequatur sed ipsa amet facilis,</p><p>accusamus esse minus architecto cumque fugit voluptate. Impedit deleniti, animi architecto recusandae earum nostrum ad harum aperiam numquam ut voluptate enim. Officia consequatur, iste iusto harum nostrum aperiam incidunt nobis provident praesentium rerum saepe, quae eius voluptatem et laudantium molestias molestiae modi neque. Repudiandae, reiciendis eveniet temporibus minima nesciunt accusantium quae.</p>',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'lorem keempat dibuat',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore dolores consectetur atque iure dolor maxime dolorum expedita aliquam recusandae dolore natus rem,</p><p>ab autem pariatur magni architecto eligendi quaerat cumque incidunt corrupti? Consequuntur odit assumenda dicta veritatis vel delectus temporibus labore necessitatibus quae eum consequatur sed ipsa amet facilis,</p><p>accusamus esse minus architecto cumque fugit voluptate. Impedit deleniti, animi architecto recusandae earum nostrum ad harum aperiam numquam ut voluptate enim. Officia consequatur, iste iusto harum nostrum aperiam incidunt nobis provident praesentium rerum saepe, quae eius voluptatem et laudantium molestias molestiae modi neque. Repudiandae, reiciendis eveniet temporibus minima nesciunt accusantium quae.</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,11 @@
 @section('container')
 <h2>{{ $post->title }}</h2>
 
-<p>By. Dikz in
-  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+<p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in
+  <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
 {!! $post->body !!}
-<a href="/blog">back to Posts</a>
+<a href="/blog" class="text-decoration-none">back to Posts</a>
 @endsection
 
 
