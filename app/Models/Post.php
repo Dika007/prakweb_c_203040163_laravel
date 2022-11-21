5ce0eaf50a928487e15cbcc50964e446227b4cfa
Dika <dsa799059@gmail.com>
2022-11-21 11:22:03 +0700

    Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+
+    use HasFactory, Sluggable;
+
 
     //protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
@@ -39,6 +42,7 @@ class Post extends Model
     }
 
     public function category()
+
     {
         return $this->belongsTo(Category::class);
     }
@@ -51,5 +55,14 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
